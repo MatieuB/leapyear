189c8ed60eb498f027c465859e7fb7f574a463b9
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-02-12 08:00:49 -0700

    second commit, remote not working

--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 var code = require('../main');
 var expect = require('chai').expect;
 
-describe('', function (){
-  it('');
+describe("Leap Year", function (){
+  it('will check if leapyear by checking remainders of leapyear criteria', function (){
+    expect(code.leapYear()).to.equal(true)
+  })
 });
